a29bca308a3934c74fc26724afddddf89b159572
j-prt <peart.l.joe@gmail.com>
2024-02-07 16:15:52 -0800

    checkpoint

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,12 +1,21 @@
+import styled from 'styled-components'
 import FullScreenBackground from '../../ui/FullScreenBackground'
 import MainContainer from '../../ui/MainContainer'
 import BlogCard from './BlogCard'
+
+const Rule = styled.div`
+  margin-bottom: 6rem;
+  width: 85%;
+  border-bottom: 1px solid var(--color-accent); // rgba(228, 240, 194, 0.7); // color-accent-light
+  box-shadow: 0rem 0rem 2rem 0.05rem var(--color-accent);
+`
 
 function Blog() {
   return (
     <FullScreenBackground size='long' color='secondary'>
       <MainContainer>
         Blog
+        <Rule />
         <BlogCard />
         <BlogCard />
         <BlogCard />
